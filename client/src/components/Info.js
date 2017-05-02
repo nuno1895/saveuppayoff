28839f15fc8d1beeab15a3c145d7e4e48fac52b3
Steven Gissubel <steven.gissubel@gmail.com>
2017-05-02 18:01:49 -0400

    added info page and css

---
+++ client/src/components/Info.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import MonthlyExpenses from './monthlyExpenses'
+
+class Info extends Component {
+  render() {
+    return (
+      
+    );
+  }
+}
+
+export default Info;
